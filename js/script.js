6f680f2fa14af8b238e4180379c98dc2d663c1c0
KritskiyRomanUP-211 <kritskiyroma896@gmail.com>
2023-06-10 19:31:48 +0300

    2nd Commit

--- js/script.js
+++ js/script.js
@@ -10,3 +10,10 @@ helpLink.addEventListener("click", function(event) {
   event.preventDefault();
   helpText.style.display = "flex"
 });
+
+const dateInput = document.getElementById('dateInput');
+const errorText = document.getElementById('errorText');
+
+dateInput.addEventListener('click', function() {
+  errorText.style.display = 'block';
+});
