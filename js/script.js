644ad3f3555ccfa6967f2ae2946193d91021d199
KritskiyRomanUP-211 <kritskiyroma896@gmail.com>
2023-06-12 15:41:27 +0300

    Confirm TODO and stuff

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
+// *1st message
+const dateInput = document.getElementById('dateInput');
+const errorText = document.getElementById('errorText');
+
+dateInput.addEventListener('click', function() {
+  errorText.style.display = 'block';
+});
+
+// *2nd message
+const textInput = document.getElementById('textInput');
+const errorMessage = document.getElementById('errorMessage');
+
 function playErrorSound() {
   var audio = document.getElementById("error-sound");
   audio.play();
+  errorMessage.style.display = 'block';
 }
+  // Показати повідомлення про помилку
 
+// *3rd message
 var helpLink = document.querySelector(".help-link");
 var helpText = document.querySelector(".help-text");
 
 helpLink.addEventListener("click", function(event) {
   event.preventDefault();
   helpText.style.display = "flex"
-});
-
-const dateInput = document.getElementById('dateInput');
-const errorText = document.getElementById('errorText');
-
-dateInput.addEventListener('click', function() {
-  errorText.style.display = 'block';
 });
